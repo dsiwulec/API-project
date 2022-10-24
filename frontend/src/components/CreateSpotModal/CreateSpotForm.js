1b70844bf6ff778f6736670ad06b00ca300ccfe9
David Siwulec <david.siwulec@gmail.com>
2022-10-24 00:21:45 -0400

    Changed Demo URL button to properly set character count in input field

--- frontend/src/components/CreateSpotModal/CreateSpotForm.js
+++ frontend/src/components/CreateSpotModal/CreateSpotForm.js
@@ -162,7 +162,10 @@ function CreateSpotForm({ setShowModal }) {
                     onFocus={(e) => setCharacterCount(e.target.value.length)}
                 />
                 <div className="under-url">
-                    <button className="demo-url" type="button" onClick={() => setPreviewUrl("https://a0.muscache.com/im/pictures/d7e9ec73-f907-493d-83ed-1d89ad92fb5e.jpg?im_w=1200")}>Demo URL</button>
+                    <button className="demo-url" type="button" onClick={() => {
+                        setPreviewUrl("https://a0.muscache.com/im/pictures/d7e9ec73-f907-493d-83ed-1d89ad92fb5e.jpg?im_w=1200")
+                        setCharacterCount("https://a0.muscache.com/im/pictures/d7e9ec73-f907-493d-83ed-1d89ad92fb5e.jpg?im_w=1200".length)
+                    }}>Demo URL</button>
                     <div className="character-count">{characterCount}/255</div>
                 </div>
             </div>
